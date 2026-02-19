b842fead328c88e19e99b242babfd2ea6172ce8a
teddyfranklin <tf563@drexel.edu>
2026-02-18 22:26:39 -0500

    hwk 04

---
+++ homework/homework_04/assets/scripts/main.js
@@ -0,0 +1,10 @@
+// Form validation script
+const firstNameInput = document.getElementById('firstName');
+const firstNameError = document.getElementById('firstName-error');
+
+input.addEventListener('blur', function() {
+    const value = input.value.trim();
+    if (value === '') {
+        
+    }
+});
